5f373f630f5880cb77c5ed309c4395c5caf7c1ff
FoxIS <andrius.mikonis@gmail.com>
2018-12-15 20:13:40 +0200

    FIX: Processors display results, etc.

--- EasyVision/processors/base.py
+++ EasyVision/processors/base.py
@@ -7,37 +7,61 @@ class ProcessorBase(VisionBase):
     def __init__(self, vision, enabled=True, *args, **kwargs):
         if not isinstance(vision, VisionBase):
             raise TypeError("Vision object must be of type VisionBase")
-        super(ProcessorBase, self).__init__(*args, **kwargs)
         self._vision = vision
         self._enabled = enabled
+        super(ProcessorBase, self).__init__(*args, **kwargs)
 
     @abstractmethod
-    def process(self, frame):
+    def process(self, image):
         pass
 
     def capture(self):
-        return self.process(self._vision.capture()) if self._enabled else self._vision.capture()
+        frame = self._vision.capture()
+        if not self.enabled:
+            return frame
+        else:
+            images = tuple(self.process(img) for img in frame.images)
+            return frame._replace(images=images)
+
+    def release(self):
+        self._vision.release()
 
     @property
-    def enabled(self):     return self._enabled
-
-    @property.setter
-    def enabled(self, value):     self._enabled = value
+    def source(self):
+        return self._vision
 
     @property
-    def is_open(self):     return self._vision.is_open
+    def enabled(self):
+        return self._enabled
+
+    @enabled.setter
+    def enabled(self, value):
+        self._enabled = value
 
     @property
-    def frame_size(self):     return self._vision.frame_size
+    def is_open(self):
+        return self._vision.is_open
 
     @property
-    def fps(self):     return self._vision.fps
+    def frame_size(self):
+        return self._vision.frame_size
 
     @property
-    def frame_count(self):     return self._vision.frame_count
+    def fps(self):
+        return self._vision.fps
 
     @property
-    def path(self):    return self._vision.path
+    def name(self):
+        return "{} <- {}".format(super(ProcessorBase, self).name, self._vision.name)
 
     @property
-    def devices(self):    return self._vision.devices
+    def frame_count(self):
+        return self._vision.frame_count
+
+    @property
+    def path(self):
+        return self._vision.path
+
+    @property
+    def devices(self):
+        return self._vision.devices
---
+++ EasyVision/processors/featureextractor.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+import cv2
+import numpy as np
+from .base import *
+
+
+class FeatureExtraction(ProcessorBase):
+
+    def __init__(self, vision, feature_type, debug=False, display_results=False, *args, **kwargs):
+        if feature_type == 'ORB':
+            defaults = dict(nfeatures=10000, scoreType=cv2.ORB_FAST_SCORE)
+            defaults.update(kwargs)
+            self._descriptor = cv2.ORB_create(**defaults)
+        elif feature_type == 'KAZE':
+            defaults = dict()
+            defaults.update(kwargs)
+            self._descriptor = cv2.KAZE_create(**kwargs)
+        elif feature_type == 'AKAZE':
+            defaults = dict()
+            defaults.update(kwargs)
+            self._descriptor = cv2.AKAZE_create(**kwargs)
+        else:
+            raise TypeError("Invalid feature type")
+        super(FeatureExtraction, self).__init__(vision, debug=debug, display_results=display_results, *args, **kwargs)
+
+    @property
+    def description(self):
+        return "Simple Feature Extractor processor"
+
+    def process(self, image):
+        mask = None
+        if isinstance(image, ImageWithMask):
+            mask = self._make_mask(image.mask)
+
+        keypoints = self._descriptor.detectAndCompute(image.image, mask)
+
+        if self.display_results:
+            self._draw_keypoints(image.image, keypoints[0])
+
+        if mask:
+            return ImageWithMaskAndFeatures(self, image.image, image.mask, keypoints)
+        else:
+            return ImageWithFeatures(self, image.image, keypoints)
+
+    def _make_mask(self, mask):
+        raise NotImplementedError()
+
+    def _draw_keypoints(self, image, keypoints):
+        img = cv2.drawKeypoints(image, keypoints, np.array([]), color=(0, 0, 255),
+                                 flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        cv2.imshow(self.name, img)
+
+    def display_results_changed(self, last, current):
+        if current:
+            cv2.namedWindow(self.name, cv2.WINDOW_NORMAL)
+        else:
+            cv2.destroyWindow(self.name)
