c22892817f669b05000a24c545e9825035f57238
FoxIS <andrius.mikonis@gmail.com>
2018-12-15 13:30:04 +0200

    ADD: Mocking to monocular vision tests.

--- tests/test_vision_monocular.py
+++ tests/test_vision_monocular.py
@@ -4,24 +4,63 @@
 import pytest
 from pytest import raises, approx
 from EasyVision.vision import *
+import cv2
 
-def test_monocular_vision():
+
+class VideoCaptureMockClass(object):
+
+    def __init__(self, dev):
+        self._dev = dev
+        self._isOpened = dev == 0
+
+    def isOpened(self): return self._isOpened
+
+    def set(self, propr, val): pass
+
+    def get(self, porp): return 0
+
+    def read(self):
+        return self.isOpened(), "image"
+
+    def release(self): pass
+
+
+def VideoCaptureMock(dev):
+    return VideoCaptureMockClass(dev)
+
+
+def test_monocular_vision(mocker):
+    mocker.patch('cv2.VideoCapture', VideoCaptureMock)
     vision = MonocularVision(0)
 
 
-def test_monocular_vision_devicenotfound():
+def test_monocular_vision_devicenotfound(mocker):
+    mocker.patch('cv2.VideoCapture', VideoCaptureMock)
     with raises(DeviceNotFound):
-        _ = MonocularVision(999)
+        MonocularVision(999)
 
 
-def test_monocular_vision_capture():
+def test_monocular_vision_capture(mocker):
+    mocker.patch('cv2.VideoCapture', VideoCaptureMock)
     vision = MonocularVision(0)
 
     img = vision.capture()
     assert(isinstance(img, Frame))
 
 
-def test_monocular_vision_capture_debug():
-    vision = MonocularVision(0)
-    vision.debug = True
-    img = vision.capture()
+def test_monocular_vision_capture_debug(mocker):
+    mocker.patch('cv2.namedWindow', autospec=True)
+    mocker.patch('cv2.destroyWindow', autospec=True)
+    mocker.patch('cv2.imshow', autospec=True)
+    mocker.patch('cv2.VideoCapture', VideoCaptureMock)
+    with MonocularVision(0) as vision:
+        vision.debug = True
+        img = vision.capture()
+        assert(isinstance(img, Frame))
+        assert(img.images[0].source is vision)
+        assert(img.images[0].image == "image")
+
+    name = "Capture 0"
+    cv2.namedWindow.assert_called_with(name, cv2.WINDOW_NORMAL)
+    cv2.destroyWindow.assert_called_with(name)
+    cv2.imshow.assert_called_with(name, "image")
