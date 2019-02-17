405fa7fd529fd652b3a0076b6eb8e64d42ba4789
FoxIS <andrius.mikonis@gmail.com>
2019-02-17 19:03:35 +0200

    FIX: Resized map image.

--- tests/engine/test_map.py
+++ tests/engine/test_map.py
@@ -19,7 +19,7 @@ def test_map_load_plan():
     m = cv2.cvtColor(cv2.imread("test_data/gridmap.bmp"), cv2.COLOR_BGR2GRAY)
 
     start = Pose(0, [[1, 0, 0], [0, 1, 0], [0, 0, 1]], [[10], [0], [5]])
-    target = Pose(0, [[1, 0, 0], [0, 1, 0], [0, 0, 1]], [[325//2], [0], [5]])
+    target = Pose(0, [[1, 0, 0], [0, 1, 0], [0, 0, 1]], [[325], [0], [5]])
 
     _map = OccupancyGridMap(m, scale=1, poses=[start])
 
@@ -29,6 +29,6 @@ def test_map_load_plan():
     #cv2.waitKey(0)
 
     assert(tuple(path[0]) == (10, 5))
-    assert(tuple(path[-1]) == (325 // 2, 5))
+    assert(tuple(path[-1]) == (325, 5))
     assert(len(path) > 10)
 
