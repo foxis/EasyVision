a0f364ed219a4d36d7baa3ed7925d7f679d2bf79
FoxIS <andrius.mikonis@gmail.com>
2019-03-25 11:02:06 +0200

    ADD: Simple and Pyro synchronizers.

--- EasyVision/processors/synchronization.py
+++ EasyVision/processors/synchronization.py
@@ -3,6 +3,8 @@
 """
 
 from .base import *
+import Pyro4
+import multiprocessing as mp
 
 
 class Synchronize(ProcessorBase):
@@ -36,3 +38,68 @@ class Synchronize(ProcessorBase):
         if self.enabled:
             self._sync()
         return self.source.capture()
+
+
+class SimpleSynchronizer(object):
+    """Simple Multiprocessing synchronizer that uses events and locks to synchronize two processes together.
+    """
+
+    def __init__(self, num_streams, timeout=30):
+        """SimpleSynchronizer object initialization.
+
+        :param num_streams: Number of streams to synchronize
+        :param timeout: timeout for the event waiting.
+        """
+        self._events = tuple(mp.Event() for _ in range(num_streams))
+        self._lock = mp.Lock()
+        self._timeout = timeout
+        for i in self._events:
+            i.clear()
+
+    def __call__(self):
+        """Will find an unset event, set it and wait for all events.
+        After all events are set, will clear the one event that it set previously.
+        In result every caller(up to num_streams) will wait for every other caller to call this
+        synchronizer and release control to the stream afterwards.
+
+        Will raise a TimeoutError exception if timeout occurs waiting for an event.
+        """
+        with self._lock:
+            my_event = -1
+            for i, ev in enumerate(self._events):
+                if not ev.is_set():
+                    ev.set()
+                    my_event = i
+                    break
+
+        for i in self._events:
+            if i.wait(self._timeout):
+                raise TimeoutError
+
+        with self._lock:
+            self._events[my_event].clear()
+
+
+class PyroSynchronizer(object):
+    """Pyro synchronizer proxy object. Will connect to a PyroSynchronizerObject
+    specified by name and will relay synchronization task to it."""
+
+    def __init__(self, name, nameserver=None):
+        with Pyro4.locateNS(host=nameserver) as ns:
+            uri = ns.lookup(name)
+        self._proxy = Pyro4.Proxy(uri)
+
+    def __call__(self):
+        self._proxy.sync()
+
+
+class PyroSynchronizerObject(SimpleSynchronizer):
+    """A Pyro synchronizer object wrapper over SimpleSynchronizer to provide syncing functionality over Pyro"""
+
+    def __init__(self, num_streams):
+        super(PyroSynchronizerObject, self).__init__(num_streams)
+
+    @Pyro4.expose
+    def sync(self):
+        self()
+
